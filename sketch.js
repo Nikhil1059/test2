65332713eca5f2929fc75efd849833ce42797aa5
Nikhil1059 <nkarumanchi1005@gmail.com>
2020-11-11 17:48:03 +0530

    test2

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ var backgroundImg,platform;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
-
+// this is a function
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
